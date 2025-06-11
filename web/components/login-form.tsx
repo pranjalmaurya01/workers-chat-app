a3a9bfc5b64b6d9c053703223c2dbaef4223056a
pranjal maurya <pranjalmaurya01@gmail.com>
2025-06-11 15:03:25 +0530

    added join room page

---
+++ web/components/login-form.tsx
@@ -0,0 +1,128 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { cn } from '@/lib/utils';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@radix-ui/react-tooltip';
+import { RefreshCcw } from 'lucide-react';
+import { useRef, useState } from 'react';
+import { toast } from 'sonner';
+import {
+  adjectives,
+  animals,
+  colors,
+  uniqueNamesGenerator,
+} from 'unique-names-generator';
+
+const MIN_LENGTH_ROOM_NAME = 15;
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<'div'>) {
+  const [state, setState] = useState({ code: '' });
+  const codeRef = useRef<HTMLInputElement | null>(null);
+
+  function returnUniqueName() {
+    const shortName = uniqueNamesGenerator({
+      dictionaries: [adjectives, colors, animals],
+      style: 'capital',
+      separator: '',
+    });
+
+    if (shortName.length < MIN_LENGTH_ROOM_NAME) {
+      return returnUniqueName();
+    }
+
+    return shortName;
+  }
+
+  function createNewRoom() {
+    setState((prev) => ({ ...prev, code: returnUniqueName() }));
+  }
+
+  return (
+    <div className={cn('flex flex-col gap-6', className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Join a room</CardTitle>
+          <CardDescription>
+            Enter a room code or create a new room
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+
+              if (state.code.length < MIN_LENGTH_ROOM_NAME) {
+                toast.error('Code too short', {});
+              }
+            }}
+          >
+            <div className='flex flex-col gap-6'>
+              <div className='flex w-full max-w-sm items-end gap-2'>
+                <div className='flex justify-between w-full'>
+                  <div className='grid gap-1 w-full'>
+                    <Label htmlFor='code'>Code</Label>
+                    <Input
+                      id='code'
+                      required
+                      className=''
+                      value={state.code}
+                      onChange={(e) => {
+                        setState((prev) => ({ ...prev, code: e.target.value }));
+                      }}
+                    />
+                  </div>
+                </div>
+
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      type='button'
+                      variant={'outline'}
+                      onClick={() => {
+                        createNewRoom();
+                      }}
+                    >
+                      <RefreshCcw />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p className='text-sm'>Generate New Room Code</p>
+                  </TooltipContent>
+                </Tooltip>
+              </div>
+
+              <div className='flex flex-col gap-3'>
+                <div></div>
+                <div>
+                  <Button
+                    type='submit'
+                    className='w-full'
+                    disabled={state.code.length < MIN_LENGTH_ROOM_NAME}
+                  >
+                    Join
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
