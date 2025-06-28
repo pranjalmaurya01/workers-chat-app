74dc16d0163bba2d104c3cd84a2f114a27060eed
Pranjal Maurya <pranjalmaurya01@gmail.com>
2025-06-28 13:46:28 +0530

    fix: added suspense boundry

--- web/app/chat/page.tsx
+++ web/app/chat/page.tsx
@@ -1,9 +1,12 @@
 import ChatArea from '@/app/chat/chat-area';
+import { Suspense } from 'react';
 
 export default function () {
   return (
     <div className='flex h-screen bg-gray-100'>
-      <ChatArea />
+      <Suspense fallback={null}>
+        <ChatArea />
+      </Suspense>
     </div>
   );
 }
